f295a36ac85a78ceb746ab10cae9d463b1a01018
artem9ev <zemla357@gmail.com>
2024-04-25 19:30:38 +0300

    added some base files

---
+++ logic.php
@@ -0,0 +1,15 @@
+
+<?php
+echo 'sdaxzc';
+$text = 'text';
+function ShowForm1(){
+    include('form.php');
+}
+// ob_start();
+// echo $text;
+// include('form.php');
+// $content = ob_get_contents();
+// ob_flush();
+// ob_end_clean();
+// ob_end_flush()
+?>
