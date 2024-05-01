16ed2095037c7adec738ff9c2c9832e7b156f579
artem9ev <zemla357@gmail.com>
2024-05-01 13:05:55 +0300

    Update index.php

--- index.php
+++ index.php
@@ -38,11 +38,13 @@ elseif ($_SERVER['REQUEST_METHOD'] == 'POST'){
             $select = "select * from Products where substr(name_prod, 1, $len) = '$name';";
             $result = $db->query($select);
             $table_data[] = array('id', 'name', 'price'); // добавляю первую строку в таблицу
+            echo "Пытаюсь взять столбцы<br>";
             while($row = $result->fetch()){ // прохожу каждую строку таблицы из бд, которую получил в результате запроса
                 $table_data[] = array($row['id_prod'], $row['name_prod'], $row['price_prod']);
             }
             setcookie('table', serialize($table_data)); // сохраняю табличку в куки
             $mas[] = "Успешно полученно";
+            echo "Успешно полученно<br>";
         }
         else if($_POST["form_name"] == 'form_3'){
     
@@ -50,6 +52,7 @@ elseif ($_SERVER['REQUEST_METHOD'] == 'POST'){
     }
     catch(PDOException $e){
         $mas[] = 'Error : ' . $e->getMessage();
+        echo 'Error : ' . $e->getMessage() . '<br>';
         exit();
     }
     setcookie('form_name', $_POST['form_name']);
