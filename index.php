18f080bbdfda8089ebc99a3b4e7c258e3cd4861e
artem9ev <zemla357@gmail.com>
2024-05-01 13:13:44 +0300

    Update index.php

--- index.php
+++ index.php
@@ -27,12 +27,6 @@ elseif ($_SERVER['REQUEST_METHOD'] == 'POST'){
     $mas = array();
     $table_data = array(); // делаю массив для сохранения таблицы из бд
     try {
-        $conn = new PDO("mysql:host=localhost;dbname=$dbname", $username, $password);
-        echo "Database connection<br>";
-    } catch (PDOException $e) {
-        echo "Connection failed: " . $e->getMessage() . '<br>';
-    }
-    try {
         $db = new PDO("mysql:host=localhost;dbname=$dbname", $username, $password,
         [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
         if($_POST["form_name"] == 'form_1'){
@@ -44,17 +38,11 @@ elseif ($_SERVER['REQUEST_METHOD'] == 'POST'){
             $select = "select * from Products where substr(name_prod, 1, $len) = '$name';";
             $result = $db->query($select);
             $table_data[] = array('id', 'name', 'price'); // добавляю первую строку в таблицу
-            echo 'Пытаюсь взять строку<br>';
             while($row = $result->fetch()){ // прохожу каждую строку таблицы из бд, которую получил в результате запроса
-                $newrow = array();
-                $newrow[] = $row['id_prod']; // присваиваю значения по именам столбцов из таблицы в бд
-                $newrow[] = $row['name_prod'];
-                $newrow[] = $row['price_prod'];
-                $table_data[] = $newrow;
+                $table_data[] = array($row['id_prod'], $row['name_prod'], $row['price_prod']);
             }
             setcookie('table', serialize($table_data)); // сохраняю табличку в куки
             $mas[] = "Успешно полученно";
-            echo 'Успешно полученно<br>';
         }
         else if($_POST["form_name"] == 'form_3'){
     
