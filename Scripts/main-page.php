0ce1270897624b113e747f0c3a2a28b621491fbb
artem9ev <zemla357@gmail.com>
2024-05-12 20:04:24 +0300

    Update main-page.php

--- Scripts/main-page.php
+++ Scripts/main-page.php
@@ -71,7 +71,19 @@ header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
             </div>
         </div>
     </div>
-
+    <div class="table-item"> 
+                <div class="">
+                    <table>
+                        <?php foreach($table as $row){ // прохожусь по массиву, доставая из него табличку
+                            echo '<tr>';
+                            foreach($row as $cell){
+                                echo '<td>' . $cell . '</td>';
+                            }
+                            echo '</tr>';
+                        } ?>
+                    </table>
+                </div>
+            </div>
     
 
     <script>SelectFormOnGet()</script>
