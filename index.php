53815f603b1b64b96f8627fff81c3e2f98ef121c
artem9ev <zemla357@gmail.com>
2024-05-12 21:50:38 +0300

    Update index.php

--- index.php
+++ index.php
@@ -31,8 +31,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET'){
         if($formName == 'form_1'){
 
         }
-        else if($formName == 'form_2'){
-            $len = strlen($_COOKIE['product']);
+        else if($formName == 'form_2' && !empty($_COOKIE['product'])){
+            $len = strlen($_COOKIE['product']) / 2;
             $name = $_COOKIE['product'];
             echo "prod $len:$name<br>";
             $select = "select * from Products WHERE SUBSTR(name, 1, $len) = '$name';";
