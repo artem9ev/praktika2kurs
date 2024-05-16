32d859fb1e8b7ac277855081739d9c0d7060f2a0
artem9ev <zemla357@gmail.com>
2024-05-16 09:37:05 +0300

    Update index.php

--- index.php
+++ index.php
@@ -110,7 +110,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET'){
                 $date2 = $_COOKIE['date2'];
             }
             $select = "SELECT p.name, o.name, p.buy_price, p.sale_price, s.number_of_units, sm.full_name, s.sale_date 
-            FROM Products p, Salesmans sm, Sales s, Provider o
+            FROM Products p, Salesmans sm, Sales s, Providers o
             WHERE p.id = s.product_id AND sm.id = s.saleman_id AND o.id = p.provider_id AND s.sale_date BETWEEN '$date1' AND '$date2';";
             $result = $db->query($select);
             $tableString = array('НАЗВАНИЕ', 'ЦЕНА ЗАКУПКИ', 'ЦЕНА ПРОДАЖИ', 'КОЛИЧЕСТВО', 'ПРОДАВЕЦ', 'ДАТА'); // добавляю первую строку в таблицу
